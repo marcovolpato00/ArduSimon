cf6a0ebe447cb30700719c5c65559d113a9e2523
marcovolpato00 <marco.volpato00@gmail.com>
2018-03-02 21:25:04 +0100

    Now it works as it should

--- ArduSimon.ino
+++ ArduSimon.ino
@@ -31,8 +31,12 @@
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
 const int startLevel = 1;
-const int maxLevel = 20;
+const int maxLevel = 5;
 const int speed = 500;
+
+int buttonPin[4] = { YELLOW_BUTTON, GREEN_BUTTON, BLUE_BUTTON, RED_BUTTON };
+int ledPin[4] = { YELLOW_LED, GREEN_LED, BLUE_LED, RED_LED };
+String ledPinNames[4] = { "Yellow", "Green", "Blue", "Red" };
 
 int userSequence[maxLevel];
 int sequence[maxLevel];
@@ -55,50 +59,159 @@ void setup()
 	lcd.begin(16, 2);
 	Serial.begin(9600);
 
+	generateSequence();
 	lcd.print("ArduSimon");
 	delay(1000);
-	countdown();
 }
 
 void loop()
 {
 	if (level <= maxLevel)
 	{
-		generateSequence();
+		lcdPrint("Livello " + String(level), 0);
+		delay(1000);
+		countdown();
+
 		showSequence();
-		//Serial.println("Inserisci la sequenza");
 		lcdPrint("Inserisci la", 0);
 		lcdPrint("sequenza", 1);
 		getSequence();
-
-		if (sequenceIsCorrect())
-		{
-			//Serial.println("Sequenza corretta!");
-			lcdPrint("Sequenza", 0);
-			lcdPrint("corretta!", 1);
-			flashAll();
-			level++;
-			lcdPrint("Livello " + String(level), 0);
-			delay(1000);
-		}
-		else
-		{
-			//Serial.println("Sequenza non corretta! Riprova");
-			lcdPrint("Sequenza non", 0);
-			lcdPrint("corretta...", 1);
-			delay(1000);
-			level = startLevel;
-			lcdPrint("Livello " + String(level), 0);
-			delay(1000);
-		}
-		
-		countdown();
 	}
+	else
+	{
+		finalFlash();
+		lcdPrint("ArduSimon", 0);
+	}
+}
+
+bool sequenceIsCorrect(int counter)		// check if the sequence is correct
+{
+	if (userSequence[counter] != sequence[counter])
+		return false;
+	return true;
+}
+
+void sequenceCorrect()		// runs when the sequence is correct
+{
+	lcdPrint("Sequenza", 0);
+	lcdPrint("corretta!", 1);
+	flashAll();
+	level++;
+	delay(1000);
+}
+
+void sequenceWrong()		// runs when the sequence is wrong
+{
+	lcdPrint("Sequenza non", 0);
+	lcdPrint("corretta...", 1);
+	level = startLevel;
+	delay(1000);
+}
+
+void getSequence()
+{
+	bool buttonPressed;
+
+	for (int i = 0; i < level; i++)
+	{
+		buttonPressed = false;
+
+		while (!buttonPressed)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				if (digitalRead(buttonPin[j]) == LOW)
+				{
+					int led = ledPin[j];
+					flash(led);
+					userSequence[i] = led;
+
+					buttonPressed = true;
+					
+					if (!sequenceIsCorrect(i))
+					{
+						sequenceWrong();
+						return;				// return to main loop
+					}
+				}
+			}
+		}
+	}
+
+	sequenceCorrect();
+}
+
+void showSequence()
+{
+	turnOffAll();
+
+	for (int i = 0; i < level; i++)
+	{
+		digitalWrite(sequence[i], HIGH);
+		delay(speed);
+		digitalWrite(sequence[i], LOW);
+		delay(speed);
+	}
+}
+
+void generateSequence()
+{
+	randomSeed(analogRead(A0));
+
+	for (int i = 0; i < maxLevel; i++)
+	{
+		sequence[i] = random(2, 6);
+	}
+}
+
+void turnOffAll()
+{
+	digitalWrite(YELLOW_LED, LOW);
+	digitalWrite(GREEN_LED, LOW);
+	digitalWrite(BLUE_LED, LOW);
+	digitalWrite(RED_LED, LOW);
+}
+
+void flash(int led)
+{
+	digitalWrite(led, HIGH);
+	delay(200);
+	digitalWrite(led, LOW);
+  delay(200);
+}
+
+void flashAll()
+{
+	for (int i = 1; i <= 4; i++)
+	{
+		digitalWrite(YELLOW_LED, HIGH);
+		digitalWrite(GREEN_LED, HIGH);
+		digitalWrite(BLUE_LED, HIGH);
+		digitalWrite(RED_LED, HIGH);
+		delay(200);
+		turnOffAll();
+		delay(200);
+	}
+}
+
+void finalFlash()
+{
+	digitalWrite(YELLOW_LED, HIGH);
+	delay(200);
+	digitalWrite(YELLOW_LED, LOW);
+	digitalWrite(GREEN_LED, HIGH);
+	delay(200);
+	digitalWrite(GREEN_LED, LOW);
+	digitalWrite(BLUE_LED, HIGH);
+	delay(200);
+	digitalWrite(BLUE_LED, LOW);
+	digitalWrite(RED_LED, HIGH);
+	delay(200);
+	digitalWrite(RED_LED, LOW);
 }
 
 void lcdPrint(String message, int row)
 {
-
 	if (row == 1)
 	{
 		lcd.setCursor(0, 1);
@@ -121,122 +234,4 @@ void countdown()
 	lcdPrint("1", 0);
 	delay(1000);
 	lcd.clear();
-}
-
-void buttonPressed(int led, int counter)	// when a button is pressed
-{
-	flash(led);
-	Serial.println(ledToString(led));
-	userSequence[counter] = led;
-}
-
-void getSequence()
-{
-	for (int i = 0; i < level; i++)
-	{
-		while (true)
-		{
-			if (digitalRead(YELLOW_BUTTON) == LOW)
-			{
-				buttonPressed(YELLOW_LED, i);
-				break;
-			}
-			else if (digitalRead(GREEN_BUTTON) == LOW)
-			{
-				buttonPressed(GREEN_LED, i);
-				break;
-			}
-			else if (digitalRead(BLUE_BUTTON) == LOW)
-			{
-				buttonPressed(BLUE_LED, i);
-				break;
-			}
-			else if (digitalRead(RED_BUTTON) == LOW)
-			{
-				buttonPressed(RED_LED, i);
-				break;
-			}
-		}
-	}
-}
-
-bool sequenceIsCorrect()
-{
-	for (int i = 0; i < level; i++)
-	{
-		if (userSequence[i] != sequence[i])
-			return false;
-	}
-	return true;
-}
-
-void showSequence()
-{
-	turnOffAll();
-
-	for (int i = 0; i < level; i++)
-	{
-		digitalWrite(sequence[i], HIGH);
-		delay(speed);
-		digitalWrite(sequence[i], LOW);
-		delay(speed);
-		Serial.println(ledToString(sequence[i]));
-	}
-}
-
-String ledToString(int led)
-{
-	switch (led)
-	{
-		case 2:
-			return "Giallo";
-		case 3:
-			return "Verde";
-		case 4:
-			return "Blu";
-		case 5:
-			return "Rosso";
-		default:
-			break;
-	}
-}
-
-void generateSequence()
-{
-	randomSeed(millis());
-
-	for (int i = 0; i < maxLevel; i++)
-	{
-		sequence[i] = random(2, 6);
-	}
-}
-
-void turnOffAll()
-{
-	digitalWrite(YELLOW_LED, LOW);
-	digitalWrite(GREEN_LED, LOW);
-	digitalWrite(BLUE_LED, LOW);
-	digitalWrite(RED_LED, LOW);
-}
-
-void flash(int led)
-{
-	digitalWrite(led, HIGH);
-	delay(500);
-	digitalWrite(led, LOW);
-	delay(500);
-}
-
-void flashAll()
-{
-	for (int i = 1; i <= 4; i++)
-	{
-		digitalWrite(YELLOW_LED, HIGH);
-		digitalWrite(GREEN_LED, HIGH);
-		digitalWrite(BLUE_LED, HIGH);
-		digitalWrite(RED_LED, HIGH);
-		delay(200);
-		turnOffAll();
-		delay(200);
-	}
 }
